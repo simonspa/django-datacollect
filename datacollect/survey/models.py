d952fce3a973e774d41c8336138c97be116cefc6
Simon Spannagel <simon.spannagel@desy.de>
2016-05-22 21:54:05 +0200

    Add first almost empty draft of data model

--- datacollect/survey/models.py
+++ datacollect/survey/models.py
@@ -3,3 +3,20 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Record(models.Model):
+    name = models.CharField(max_length=500)
+    branch = models.CharField(max_length=500)
+    GENDER_CHOICES = (
+      ("M", "Male"),
+      ("F", "Female"),
+      ("O", "Other"),
+      ("?", "Unknown")
+    )
+    gender = models.CharField(
+        max_length=1,
+        choices=GENDER_CHOICES,
+        default="?"
+    )
+    date_of_birth = models.DateField(null=True, blank=True)
+    notes = models.TextField(blank=True)
