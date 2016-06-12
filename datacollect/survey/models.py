d841f9ea305301867fffae50c01b32eb72a1c70c
Simon Spannagel <simon.spannagel@desy.de>
2016-06-12 22:05:48 +0200

    Add unicode model instance name (person id plus name)

--- datacollect/survey/models.py
+++ datacollect/survey/models.py
@@ -11,6 +11,9 @@ def update_filename(instance, filename):
 
 class Record(models.Model):
 
+    def __unicode__(self):
+        return "%s (%s)" % (self.person_id, self.name)
+    
     # Additional model valiation
 
     def clean(self):
