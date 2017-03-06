78719784dbad7694fb7109f4e933a928125df183
Simon Spannagel <simon.spannagel@cern.ch>
2017-03-06 21:34:38 +0100

    Move admin actions into followupadmin class

--- datacollect/questionnaire/admin.py
+++ datacollect/questionnaire/admin.py
@@ -8,34 +8,11 @@ from django.db.utils import IntegrityError
 from django.utils.encoding import force_bytes
 from reversion.admin import VersionAdmin
 from django.db import transaction
-
-def export_urls(modeladmin, request, queryset):
-    import csv
-    from django.utils.encoding import smart_str
     
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename=survey.csv'
-    writer = csv.writer(response, csv.excel)
-    response.write(u'\ufeff'.encode('utf8'))
-
-    # Write out the column names, read from first item
-    writer.writerow(['case.person_id', 'unique_id','url'])
-
-    # Loop over requested records and write out data
-    for obj in queryset:
-        writer.writerow([obj.case.person_id, obj.unique_id, request.META['HTTP_HOST'] + "/submit/" + str(obj.unique_id)])
-        
-    return response
-export_urls.short_description = u"Export URLs"
-
-def invalidate_form(modeladmin, request, queryset):
-    queryset.update(is_answered=True)
-invalidate_form.short_description = u"Mark selected Follow-Up form as invalid"
-
 class FollowUpAdmin(VersionAdmin):
     list_display = ("person_id","name","country","date_intervention","unique_id","language","timestamp","is_answered",)
     list_filter = ("is_answered","language",)
-    actions = [invalidate_form, export_urls]
+    actions = ['mark_as_answered', 'export_urls']
 
     def person_id(self, x):
         return x.case.person_id
@@ -52,6 +29,29 @@ class FollowUpAdmin(VersionAdmin):
     def date_intervention(self, x):
         return x.case.date_intervention
     date_intervention.short_description = 'Date of the intervention'
+
+    def mark_as_answered(self, request, queryset):
+        queryset.update(is_answered=True)
+    mark_as_answered.short_description = u"Mark selected follow-up form as answered"
+
+    def export_urls(self, request, queryset):
+        import csv
+        from django.utils.encoding import smart_str
+        
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=survey.csv'
+        writer = csv.writer(response, csv.excel)
+        response.write(u'\ufeff'.encode('utf8'))
+
+        # Write out the column names, read from first item
+        writer.writerow(['case.person_id', 'unique_id','url'])
+
+        # Loop over requested records and write out data
+        for obj in queryset:
+            writer.writerow([obj.case.person_id, obj.unique_id, request.META['HTTP_HOST'] + "/submit/" + str(obj.unique_id)])
+        
+        return response
+    export_urls.short_description = u"Export URLs"
 
 admin.site.register(FollowUp, FollowUpAdmin)
 
