e9f510ef4e069467ce8689c3f511fc5726982255
Simon Spannagel <simon.spannagel@cern.ch>
2017-04-02 20:18:10 +0200

    Rephrase language selection, add flags, update language files

--- datacollect/questionnaire/forms.py
+++ datacollect/questionnaire/forms.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from django import forms
 from django.utils import timezone
 from django.core.validators import validate_email
@@ -10,25 +11,18 @@ from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
 
 class LanguageForm(forms.Form):
-    language = forms.ChoiceField(choices = settings.LANGUAGES, required=True, label=_('Choose preferred language'))
+    language = forms.ChoiceField(choices = settings.LANGUAGES, required=True, label=_('Change language / Cambiar el idioma / Changer la langue'))
 
     def __init__(self, *args, **kwargs):
         super(LanguageForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
         self.helper.form_class = 'form-horizontal'
-        self.helper.label_class = 'col-sm-3 col-md-3 col-lg-3'
+        self.helper.label_class = 'col-sm-5 col-md-5 col-lg-5'
         self.helper.field_class = 'col-sm-4 col-md-4 col-lg-4'
         self.helper.layout = Layout(
-            Div(
-                Div(
-                    HTML(_("<p>The <strong>language of this questionnaire</strong> has been chosen according to the the defender's country of origin. However, feel free to change the language prior to filling the questionnaire. Changing the language will reset the content of the questionnaire.</p>")),
-                    FieldWithButtons(
-                        'language',
-                        StrictButton('Change language <span class="glyphicon glyphicon-refresh"></span>', type='submit', name='change_language', css_class="btn-success"),
-                    ),
-                    css_class = 'panel-body'
-                ),
-                css_class = 'panel panel-primary'
+            FieldWithButtons(
+                'language',
+                StrictButton('Change language <span class="glyphicon glyphicon-refresh"></span>', type='submit', name='change_language', css_class="btn-success"),
             ),
         )
 
