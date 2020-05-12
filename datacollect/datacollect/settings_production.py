cbb28dfdb09398ad03e3cef76fafa944de3c501e
Simon Spannagel <simon.spannagel@cern.ch>
2020-05-12 20:27:25 +0200

    Fix gunicorn starter and static file storage

--- datacollect/datacollect/settings_production.py
+++ datacollect/datacollect/settings_production.py
@@ -167,10 +167,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
-STATICFILES_DIRS = ( os.path.join('static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATICFILES_DIRS = ( os.path.join('static'), )
 
 # Settings for django-bootstrap3
 BOOTSTRAP3 = {
